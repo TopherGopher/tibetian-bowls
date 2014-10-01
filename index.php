59782c928315eaf9729308c8addf422bbcb4c024
Chris Sterling <chrissterlin@gmail.com>
2014-10-01 01:42:37 -0600

    Adding primary file to be served from.

---
+++ index.php
@@ -0,0 +1,129 @@
+<html>
+<head>
+<script src="buzz.js"></script>
+<script>
+// var mySound = new buzz.sound("tk-421", {
+// 	formats: ["mp3"]
+// });
+
+var t_sound = new buzz.sound("tibetian/001-Prasantatma/001-Bowl", {
+	formats: ["wav"]
+});
+//mySound.play();
+
+//t_sound.play();
+
+var sounds = [
+
+new buzz.sound("tibetian/004-EFx/006-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/001-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/001-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/003-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/012-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/004-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/009-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/008-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/012-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/010-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/004-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/014-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/005-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/002-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/009-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/006-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/011-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/003-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/007-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/005-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/013-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/007-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/011-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/008-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/010-Pras-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/004-EFx/002-Atm-EFx", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/005-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/008-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/012-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/001-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/010-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/009-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/006-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/004-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/007-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/002-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/011-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/003-Atmananda/003-Atmananda", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/020-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/001-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/026-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/035-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/003-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/002-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/034-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/031-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/039-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/007-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/019-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/014-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/033-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/037-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/029-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/010-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/023-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/011-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/032-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/012-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/025-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/015-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/017-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/013-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/040-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/030-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/018-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/006-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/036-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/008-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/022-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/024-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/005-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/016-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/027-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/004-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/028-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/038-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/009-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/001-Prasantatma/021-Bowl", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/003-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/004-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/014-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/013-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/019-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/016-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/008-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/005-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/010-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/012-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/018-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/002-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/015-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/021-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/006-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/011-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/009-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/001-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/020-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/007-Akasha", {formats: ["wav"]}),
+new buzz.sound("tibetian/002-Akasha/017-Akasha", {formats: ["wav"]}),
+];
+
+for (var i=0; i<sounds.length; i++)
+{
+	console.log("Playing sounds[" + i + "]");
+	sounds[i].play().loop();
+}
+</script>
+</head>
+<body>
+Create a canvas and get the position - depending on actions (determined by vector math), play different tibetan sounds.
+</body>
+</html>
